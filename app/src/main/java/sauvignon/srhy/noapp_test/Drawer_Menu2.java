c743e6cf0f12bac292c495f515e41c06088a327d
yoppyopp <yoppyopp@naver.com>
2015-08-07 22:17:12 +0900

    첫 커밋

---
+++ app/src/main/java/sauvignon/srhy/noapp_test/Drawer_Menu2.java
@@ -0,0 +1,21 @@
+package sauvignon.srhy.noapp_test;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by SeungYeop on 2015-08-07.
+ */
+public class Drawer_Menu2 extends Fragment{
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.drawer_menu2, container, false);
+        return rootview;
+    }
+}
